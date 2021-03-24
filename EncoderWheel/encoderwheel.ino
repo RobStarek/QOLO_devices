c1de324a8e2dc99555fb55359f8ea86cad29bddb
RobStarek <60106783+RobStarek@users.noreply.github.com>
2021-03-24 16:08:09 +0100

    Update encoderwheel.ino

--- EncoderWheel/encoderwheel.ino
+++ EncoderWheel/encoderwheel.ino
@@ -3,10 +3,9 @@
 int PWLUT[16] = {2040, 1970, 1832, 1901, 1554, 1624, 1762, 1693, 1000, 1069, 1208, 1138, 1485, 1416, 1277, 1346};
 
 int i = 0;
-int incomingByte = 0;
+byte incomingByte = 0;
 bool valid1 = false;
 byte index = 0;
-int dt = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -15,6 +14,7 @@ void setup() {
   pinMode(SERVO_PIN, OUTPUT); 
   digitalWrite(LED_BUILTIN, 0); 
   digitalWrite(SERVO_PIN, 0); 
+  i = 0;
   Serial.begin(9600); 
 }
 
@@ -22,15 +22,19 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
           incomingByte = Serial.read();        
-          dt = int(512 + 8.0*(float(incomingByte)));
-          Serial.println(incomingByte);
-          Serial.println(dt);
+          if ((incomingByte & 0b11110000) == 0b10100000){
+            index = incomingByte & 0b00001111;
+            digitalWrite(LED_BUILTIN, index % 2);
+            digitalWrite(8, HIGH);
+            delayMicroseconds(PWLUT[index]);
+            digitalWrite(8, LOW);
+            delay(5);
+          };
+          if ((incomingByte) == 0b01011111){
+            index = incomingByte & 0b00001111;
+            digitalWrite(LED_BUILTIN, 0);
+            Serial.print("EncoderWheel\r\n");
+          }; 
   }
-  
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(8, HIGH);
-  delayMicroseconds(dt);
-  digitalWrite(8, LOW);
-  digitalWrite(LED_BUILTIN, LOW);  
-  delay(15);  
+  delay(5);  
 }
