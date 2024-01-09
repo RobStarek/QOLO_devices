98237448b99d8cabcf701579cebdda2fd76c62b5
RobStarek <60106783+RobStarek@users.noreply.github.com>
2024-01-09 10:02:35 +0100

    Update swabian_on_the_fly_coincidence_counting.py
    * Added support for coincidence pattern histogram.
    * Fixed numba deprecation warnings with reflected list.

--- Swabian/swabian_on_the_fly_coincidence_counting.py
+++ Swabian/swabian_on_the_fly_coincidence_counting.py
@@ -4,10 +4,14 @@ histogram of coincidence order from time tags.
 
 Author:
 starek.robert@gmail.com
+
+V: 1.1
 """
 
 import numpy as np
 import numba
+import numba.typed
+import numba.types
 import TimeTagger
 
 # Timetagger format
@@ -41,7 +45,7 @@ def numba_ham32(i):
 class CustomCoincidenceOrder(TimeTagger.CustomMeasurement):
     """
     Custom measurement class for on-the-fly counting histogram of coincidence order.
-    Warning: it does not support big virtual delays between channel because the 
+    Warning: it does not support big virtual delays between channel because the
     coincidence tag should always come in chronological order.
     """
 
@@ -49,18 +53,21 @@ class CustomCoincidenceOrder(TimeTagger.CustomMeasurement):
         TimeTagger.CustomMeasurement.__init__(self, tagger)
         self.n_channels = len(channels)
         self.binwidth = binwidth
-        # The method register_channel(channel) activates
-        # that data from the respective channels is transferred
-        # from the Time Tagger to the PC.
         self.channels = channels
+        self.channel_lut = numba.typed.Dict.empty(
+            key_type=numba.types.int64,
+            value_type=numba.types.int64,
+        )
+        for i, j in enumerate(self.channels):
+            self.channel_lut[j] = i
+        self.channels = np.array(self.channels, dtype=np.int64)
 
         for channel_number in channels:
             self.register_channel(channel=channel_number)
 
-        self.clear_impl()
+        self.last_timestamp = 0
 
-        # At the end of a CustomMeasurement construction,
-        # we must indicate that we have finished.
+        self.clear_impl()
         self.finalize_init()
 
     def __del__(self):
@@ -69,8 +76,6 @@ class CustomCoincidenceOrder(TimeTagger.CustomMeasurement):
         self.stop()
 
     def getData(self):
-        # Acquire a lock this instance to guarantee that process() is not running in parallel
-        # This ensures to return a consistent data.
         with self.mutex:
             return self.histogram.copy()
 
@@ -92,7 +97,6 @@ class CustomCoincidenceOrder(TimeTagger.CustomMeasurement):
         self.last_timestamp = 0
 
     def on_start(self):
-        # The lock is already acquired within the backend.
         pass
 
     def on_stop(self):
@@ -111,14 +115,14 @@ class CustomCoincidenceOrder(TimeTagger.CustomMeasurement):
             self.histogram,
             self.coincidence_registers_filtered,
             self.closed,
-            self.channels)
+            self.channel_lut)
 
     @staticmethod
     @numba.jit(nopython=True, nogil=True)
     def fast_process(tags, binwidth, channels,
                      coincidence_registers, t0s, t1s, valids,
                      histogram, coincidence_registers_filtered,
-                     closed, channels_list
+                     closed, channel_lut
                      ):
         """
         A precompiled version of the histogram algorithm for better performance
@@ -148,12 +152,9 @@ class CustomCoincidenceOrder(TimeTagger.CustomMeasurement):
                 continue
             timestamp = tag['time']
             channel_num = tag['channel']
-            # there has to be a better way, maybe lookup table?
-            # or dynamically precompile mapping function?
-            if channel_num in channels_list:
-                channel_id = channels_list.index(channel_num)
-            else:
-                continue
+
+            channel_id = channel_lut[channel_num]
+
             timestamp = tag['time']
             coincidence_registers_filtered = 0
             closed = 0
@@ -210,36 +211,208 @@ class CustomCoincidenceOrder(TimeTagger.CustomMeasurement):
             self.histogram,
             self.coincidence_registers_filtered,
             self.closed,
-            self.channels)
+            self.channel_lut
+        )
 
 
-if __name__ == '__main__':
-    tagger = TimeTagger.createTimeTagger()
-    # play there with settings to test the coincidence-order counting
-    #tagger.setTestSignal([1, 2, 3, 4], True)
-    tagger.setTestSignal([1, 3, 4], True)
-    # delay the stop channel by 2 ns to make sure it is later than the start
-    tagger.setInputDelay(2, 50)
-    tagger.setInputDelay(3, 100)
-    tagger.setInputDelay(4, 150)
-    CC_WINDOW = 500  # ps
-    CC_CHANNELS = [1, 2, 3, 4]
-    TEST_DURATION = int(1e12)
-    # We first have to create a SynchronizedMeasurements object to synchronize several measurements
-    with TimeTagger.SynchronizedMeasurements(tagger) as measurementGroup:
-        cc_meas = CustomCoincidenceOrder(
-            measurementGroup.getTagger(), CC_CHANNELS, CC_WINDOW)
-        rate_meas = TimeTagger.Counter(
-            measurementGroup.getTagger(), CC_CHANNELS, 1e12, int(TEST_DURATION//1e12))
-        measurementGroup.startFor(TEST_DURATION)
-        measurementGroup.waitUntilFinished()
-        index = cc_meas.getIndex()
-        coincidences = cc_meas.getData()
-        rate = rate_meas.getData()
+class CustomCoincidencePattern(TimeTagger.CustomMeasurement):
+    """
+    Custom measurement class for on-the-fly counting histogram of coincidence order.
+    Warning: it should not support big virtual delays between channel because the
+    coincidence tag should always come in chronological order. But it seems that
+    TimeTagger has some internal sorting mechanism, because when tested,
+    it works even with virtual delays.
+    """
 
-    TimeTagger.freeTimeTagger(tagger)
-    print("Coincidence order")
-    print(index)
-    print("counts on channels")
-    print(coincidences)
-    print("Total:", np.sum(rate))
+    def __init__(self, tagger, channels, binwidth=1000):
+        TimeTagger.CustomMeasurement.__init__(self, tagger)
+        self.n_channels = len(channels)
+        self.binwidth = binwidth
+        for channel_number in channels:
+            self.register_channel(channel=channel_number)
+        self.channels = channels
+        self.channel_lut = numba.typed.Dict.empty(
+            key_type=numba.types.int64,
+            value_type=numba.types.int64,
+        )
+        for i, j in enumerate(self.channels):
+            self.channel_lut[j] = i
+
+        self.channels = np.array(self.channels, dtype=np.int64)
+
+        self.last_timestamp = 0
+
+        self.clear_impl()
+        self.finalize_init()
+
+    def __del__(self):
+        self.stop()
+
+    def getData(self):
+        with self.mutex:
+            return self.histogram.copy()
+
+    def getIndex(self):
+        """Binary representation of index number specifies the coincidence pattern."""
+        arr = np.arange(0, int(2**self.n_channels))
+        return arr
+
+    def clear_impl(self):
+        # The lock is already acquired within the backend.
+        self.coincidence_registers = np.zeros(self.n_channels, dtype=np.uint32)
+        self.t0s = np.zeros(self.n_channels, dtype=np.int64)
+        self.t1s = np.zeros(self.n_channels, dtype=np.int64)
+        self.valids = np.zeros(self.n_channels, dtype=bool)
+        self.histogram = np.zeros(int(2**self.n_channels), dtype=np.uint32)
+        self.coincidence_registers_filtered = 0
+        self.closed = 0
+        self.last_timestamp = 0
+
+    def on_start(self):
+        # The lock is already acquired within the backend.
+        pass
+
+    def on_stop(self):
+        # The lock is already acquired within the backend.
+        # here maybe flush the last tag
+        last_virtual_timestamp = np.array(
+            [(0, 0, self.channels[0], self.last_timestamp+10*self.binwidth)], dtype=TAGFORMAT)
+        CustomCoincidenceOrder.fast_process(
+            last_virtual_timestamp,
+            self.binwidth,
+            self.n_channels,
+            self.coincidence_registers,
+            self.t0s,
+            self.t1s,
+            self.valids,
+            self.histogram,
+            self.coincidence_registers_filtered,
+            self.closed,
+            self.channel_lut)
+
+    @staticmethod
+    @numba.jit(nopython=True, nogil=True)
+    def fast_process(tags, binwidth, channels,
+                     coincidence_registers, t0s, t1s, valids,
+                     histogram, coincidence_registers_filtered,
+                     closed, channel_lut
+                     ):
+        """
+        A precompiled version of the histogram algorithm for better performance
+        nopython=True: Only a subset of the python syntax is supported.
+                       Avoid everything but primitives and numpy arrays.
+                       All slow operation will yield an exception
+        nogil=True:    This method will release the global interpreter lock. So
+                       this method can run in parallel with other python code
+
+        Warning: it mutates passed arrays.
+
+        Args:
+            TAGFORMAT: ndarray of tagformat dtype holding timestamps
+            coincidence_registers : uint32 ndarray holding coincidence marks
+            t0s, t1s : int64 ndarray holding register times
+            valids : bool ndarray
+            histogram : uint32 ndarray
+            coincidence_registers_filtered : int32
+            closed : int32
+        Returns:
+            last processed time stamp
+        """
+        for tag in tags:
+            # tag.type can be: 0 - TimeTag, 1- Error, 2 - OverflowBegin, 3 -
+            # OverflowEnd, 4 - MissedEvents
+            if tag['type'] != 0:
+                continue
+            timestamp = tag['time']
+            channel_num = tag['channel']
+            channel_id = channel_lut[channel_num]
+            timestamp = tag['time']
+            coincidence_registers_filtered = 0
+            closed = 0
+            for j in range(channels):
+                if (t0s[j] < timestamp < t1s[j]) and channel_id != j and valids[j]:
+                    coincidence_registers[j] = coincidence_registers[j] | (
+                        1 << channel_id)
+                if timestamp > t1s[j] and valids[j]:
+                    closed += 1
+                    coincidence_registers_filtered = \
+                        coincidence_registers_filtered | \
+                        coincidence_registers[j]
+                    coincidence_registers[j] = 0
+                    valids[j] = False
+            if closed > 0:
+                idx = coincidence_registers_filtered
+                if idx > 0:
+                    histogram[idx] += 1
+            t0s[channel_id] = timestamp
+            t1s[channel_id] = timestamp + binwidth
+            coincidence_registers[channel_id] = (1 << channel_id)
+            valids[channel_id] = True
+        return timestamp
+
+    def process(self, incoming_tags, begin_time, end_time):
+        """
+        Main processing method for the incoming raw time-tags.
+
+        The lock is already acquired within the backend.
+        self.data is provided as reference, so it must not be accessed
+        anywhere else without locking the mutex.
+
+        Parameters
+        ----------
+        incoming_tags
+            The incoming raw time tag stream provided as a read-only reference.
+            The storage will be deallocated after this call, so you must not store a reference to
+            this object. Make a copy instead.
+            Please note that the time tag stream of all channels is passed to the process method,
+            not only the ones from register_channel(...).
+        begin_time
+            Begin timestamp of the of the current data block.
+        end_time
+            End timestamp of the of the current data block.
+        """
+        self.last_timestamp = CustomCoincidencePattern.fast_process(
+            incoming_tags,
+            self.binwidth,
+            self.n_channels,
+            self.coincidence_registers,
+            self.t0s,
+            self.t1s,
+            self.valids,
+            self.histogram,
+            self.coincidence_registers_filtered,
+            self.closed,
+            self.channel_lut
+        )
+
+# Example:
+# if __name__ == '__main__':
+#     tagger = TimeTagger.createTimeTagger()
+#     # play there with settings to test the coincidence-order counting
+#     #tagger.setTestSignal([1, 2, 3, 4], True)
+#     tagger.setTestSignal([1, 3, 4], True)
+#     # delay the stop channel by 2 ns to make sure it is later than the start
+#     tagger.setInputDelay(2, 50)
+#     tagger.setInputDelay(3, 100)
+#     tagger.setInputDelay(4, 150)
+#     CC_WINDOW = 500  # ps
+#     CC_CHANNELS = [1, 2, 3, 4]
+#     TEST_DURATION = int(1e12)
+#     # We first have to create a SynchronizedMeasurements object
+#     #to synchronize several measurements
+#     with TimeTagger.SynchronizedMeasurements(tagger) as measurementGroup:
+#         cc_meas = CustomCoincidenceOrder(
+#             measurementGroup.getTagger(), CC_CHANNELS, CC_WINDOW)
+#         rate_meas = TimeTagger.Counter(
+#             measurementGroup.getTagger(), CC_CHANNELS, 1e12, int(TEST_DURATION//1e12))
+#         measurementGroup.startFor(TEST_DURATION)
+#         measurementGroup.waitUntilFinished()
+#         index = cc_meas.getIndex()
+#         coincidences = cc_meas.getData()
+#         rate = rate_meas.getData()
+#     TimeTagger.freeTimeTagger(tagger)
+#     print("Coincidence order")
+#     print(index)
+#     print("counts on channels")
+#     print(coincidences)
+#     print("Total:", np.sum(rate))
